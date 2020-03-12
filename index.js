cce7fb89b15728890839c4a19463b1946b902070
DzianisBogdan <thespirit3000@gmail.com>
2020-03-12 10:21:47 +0300

    feat: navbar functionality

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const handleClick = event => {
-    console.log(event.target);
-    
+    if (event.target.classList.contains("header-nav_item")) {
+        console.log(true);
+        let activeItem = document.querySelector(".activeItem");
+        activeItem.classList.remove("activeItem");
+        event.target.classList.add("activeItem");
+    }
+    // if (event.target.classList)
     // if (e.target.id === "increaseButton") {
     //   currentCount.innerHTML++;
     // }
     // if (e.target.id === "decreaseButton") {
     //   currentCount.innerHTML--;
     // }
-  };
-  const handleBody = () => {
+};
+const handleBody = () => {
     const body = document.querySelector("body");
     body.addEventListener("click", handleClick);
-  };
+};
 
-  const initialize = () => {
+const initialize = () => {
     handleBody();
-  };
-  window.onload = initialize;
+};
+window.onload = initialize;
