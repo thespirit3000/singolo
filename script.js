db088d2b5ec75adc95307f0532999a5267138a55
DzianisBogdan <thespirit3000@gmail.com>
2020-03-12 18:40:40 +0300

    feat: off screen functional

--- script.js
+++ script.js
@@ -7,21 +7,19 @@ const handleClick = event => {
         let activeItem = document.querySelector(".activeItem");
         activeItem.classList.remove("activeItem");
         event.target.classList.add("activeItem");
-    }
+    };
     if (event.target.classList.contains("prev")) {
         plusSlides(-1);
-
-    }
+    };
     if (event.target.classList.contains("next")) {
         plusSlides(1);
-    }
-    // if (event.target.classList)
-    // if (e.target.id === "increaseButton") {
-    //   currentCount.innerHTML++;
-    // }
-    // if (e.target.id === "decreaseButton") {
-    //   currentCount.innerHTML--;
-    // }
+    };
+    if (event.target.classList.contains("vertical_phone")) {
+        changeDisplay('.screen_vertical');
+    };
+    if (event.target.classList.contains("horizontal_phone")) {
+        changeDisplay('.screen_horizontal');
+    };
 };
 
 const handleBody = () => {
@@ -46,7 +44,7 @@ const currentSlide = (n) => {
 
 const showSlides = (n) => {
     let i;
-    let slides = document.querySelectorAll(".mySlides");    
+    let slides = document.querySelectorAll(".mySlides");
     if (n > slides.length) {
         state.slideIndex = 1
     }
@@ -58,10 +56,21 @@ const showSlides = (n) => {
     }
     slides[state.slideIndex - 1].style.display = "block";
 }
+
+const changeDisplay = (selector) => {
+    let screen = document.querySelector(selector);
+    if (screen.style.opacity === '0') {
+        screen.style.opacity = "1";
+    } else {
+        screen.style.opacity = '0';
+    };
+}
+
 const initialize = () => {
     handleBody();
     let slideIndex = 1;
     showSlides(slideIndex);
 };
+
 window.onload = initialize;
 window.onscroll = scrollHandle;
