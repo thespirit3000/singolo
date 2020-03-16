91a3c0126444f07b9ce416a8cadd4abddc70bc26
DzianisBogdan <thespirit3000@gmail.com>
2020-03-16 16:03:56 +0300

    feat: add Modal module

---
+++ modules/Modal.js
@@ -0,0 +1,67 @@
+export class Modal {
+  constructor(classes) {
+    this.classes = classes;
+    this.modal = '';
+    this.modalContent = '';
+    this.modalBtnClose = '';
+    this.modalBtnOk = '';
+    this.overlay = '';
+  }
+
+  buildModal(content) {
+    this.overlay = this.createDomNode(this.overlay, 'div', 'overlay');
+    this.modal = this.createDomNode(this.modal, 'div', 'modal');
+    this.modalContent = this.createDomNode(this.modalContent, 'div', 'modal_content');
+    this.modalBtnClose = this.createDomNode(this.modalBtnClose, 'button', 'btn', 'modal_close');
+    this.modalBtnClose.innerHTML = '<i class="fa fa-close modal_close"></i>';
+    this.modalBtnOk = this.createDomNode(this.modalBtnOk, 'button', 'btn', 'modal_ok');
+    this.modalBtnOk.innerHTML = 'OK';
+    this.setContent(content);
+    this.constructModal();
+
+
+    this.openModal();
+    this.bindHandlers();
+  }
+
+
+  createDomNode(node, element, ...classes) {
+    node = document.createElement(element);
+    node.classList.add(...classes);
+    return node;
+  }
+  setContent(content) {
+    if (typeof content == 'string') {
+      this.modalContent.innerHTML = content;
+    } else {
+      this.modalContent.innerHTML = '';
+      this.modalContent.appendChild(content);
+    }
+  }
+
+  constructModal() {
+    this.modal.appendChild(this.modalBtnClose);
+    this.modal.appendChild(this.modalContent);
+    this.modal.appendChild(this.modalBtnOk);
+    this.overlay.appendChild(this.modal);
+  }
+  openModal() {
+    document.body.append(this.overlay);
+  }
+
+  bindHandlers() {
+    this.overlay.addEventListener('click', this.closeModal);
+  }
+
+  closeModal() {
+    if (event.target.classList.contains('overlay')) {
+      document.querySelector('.overlay').remove();
+    }
+    if (event.target.classList.contains('modal_close')) {
+      document.querySelector('.overlay').remove();
+    }
+    if (event.target.classList.contains('modal_ok')) {
+      document.querySelector('.overlay').remove();
+    }
+  }
+}
